3e30b27c04db039cc64fe50012602b6d275e357a
sovlookup <gonorth@qq.com>
2025-12-28 14:50:33 +0800

    init

---
+++ src/client.ts
@@ -0,0 +1,12 @@
+import mainApp from "./main";
+import { treaty } from "@elysiajs/eden";
+
+const client = treaty<typeof mainApp>("localhost:3000");
+
+const res = await client
+  .workers({
+    name: "default",
+  })
+  .stop.post();
+
+console.log(res.data);
